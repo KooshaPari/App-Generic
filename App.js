dcb8e28758babd8f68a7031b12cbad07a75ad40f
KooshaPari <kooshapari@gmail.com>
2023-12-01 16:00:28 -0700

    v0.01

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Splash from './Splash';
-/*export default function App() {
+import Splash from './Components/Splash';
+import Accswitcher from './Components/Accswitcher';
+import Auth from './Components/Auth';
+import Home from './Components/Home';
+import Changeinfo from './Components/Changeinfo';
+import Link from './Components/Link';
+import Loadingscreen from './Components/Loadingscreen';
+import Login from './Components/Login';
+import Profile from './Components/Profile';
+import Settings from './Components/Settings';
+import Transaction from './Components/Transaction';
+/*export default function app() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -11,7 +21,7 @@ import Splash from './Splash';
 }*/
 
 export default function App() {
-  return <Splash />;
+  return <Accswitcher />;
 }
 const styles = StyleSheet.create({
   container: {
